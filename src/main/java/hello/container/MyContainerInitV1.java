bb0104307d196c1f2b4097c4460cdbb3a5fe05d0
seonhoshin <sunlike0508@gmail.com>
2024-11-17 23:05:21 +0900

    서블릿

--- src/main/java/hello/container/MyContainerInitV1.java
+++ src/main/java/hello/container/MyContainerInitV1.java
@@ -1,4 +1,16 @@
 package hello.container;
 
-public class MyContainerInitV1 {
+import java.util.Set;
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+public class MyContainerInitV1 implements ServletContainerInitializer {
+
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        System.out.println("MyContainerInitV1.onStartup");
+        System.out.println("MyContainerInitV1 c= " + c);
+        System.out.println("MyContainerInitV1 ctx= " + ctx);
+    }
 }
